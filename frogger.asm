f5581b264cdc6168c96d3e5fa901cb8edfb232b1
Ethan <ethanq918@163.com>
2021-12-08 17:14:29 -0500

    Milestone 4 completed. End of project.

--- frogger.asm
+++ frogger.asm
@@ -12,13 +12,14 @@
  # - Base Address for Display: 0x10008000 ($gp)
  # Which milestone is reached in this submission?
  # (See the assignment handout for descriptions of the milestones)
- # - Milestone 2
+ # - Milestone 3
  
  # Which approved additional features have been implemented?
  # (See the assignment handout for the list of additional features)
- # 1. (fill in the feature, if any)
- # 2. (fill in the feature, if any)
- # 3. (fill in the feature, if any)
+ # 1. Display the number of lives remaining.
+ # 2. Add a third row in each of the water and road sections.
+ # 3. Have objects in different rows move at different speeds.
+ # 4. Make objects (frog, logs, turtles, vehicles, etc) look more like the arcade version.
  # ... (add more if necessary)
  # Any additional information that the TA needs to know:
  # - (write here, if any)
@@ -65,13 +66,20 @@
 			      
 	frogInitial:    .word 3508
 
+	goalSpot:       .word 268, 272, 396, 400, 
+			      292, 296, 420, 424,
+			      316, 320, 444, 448,
+			      340, 344, 468, 472
+	lives: .word 3
+	
+	goal: .word 0, 0, 0, 0, 0
 
 	
 .text
 	lw $t0, displayAddress
 	li $t1, 0xab0000 #$t1 stores the red turtles
 	li $t2, 0x604000 #$t2 stores the color of the brown wood
-	li $t3, 0x5ecfe4 #$t3 stores the blue colour code
+	li $t3, 0x5ecfe4 #$t3 stores the blue water colour code
 	li $t4, 0x16537e # store the color of the blue safe area
 	li $t5, 0xffce3b # store the color of the frog
 	li $t6, 0x66aa33 # store the color of the green top area
@@ -134,67 +142,64 @@ respond_to_D:
 	
 # move up the frog	
 moveUp:
-	add $s0, $zero, $zero
-	add $s1, $zero, $zero
+
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
 	
-stMoveUp:
-	beq $s0, 4, initJump18
+	add $s1, $zero, $zero
 	lw $s1, frogInitial($s0)
 	addi $s1, $s1, -384
 	sw $s1, frogInitial($s0)
-	addi $s0, $s0, 4
-	j stMoveUp
 	
-initJump18:
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+	
 	jr $ra
 
 
 # move left the frog
 moveLeft:
-	add $s0, $zero, $zero
-	add $s1, $zero, $zero
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
 	
-stMoveLeft:
-	beq $s0, 4, initJump19
+	add $s1, $zero, $zero
 	lw $s1, frogInitial($s0)
 	addi $s1, $s1, -4
 	sw $s1, frogInitial($s0)
-	addi $s0, $s0, 4
-	j stMoveLeft
 	
-initJump19:
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+	
 	jr $ra
 	
 # move the frog down	
 moveDown:
-	add $s0, $zero, $zero
-	add $s1, $zero, $zero
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
 	
-stMoveDown:
-	beq $s0, 4, initJump20
+	add $s1, $zero, $zero
 	lw $s1, frogInitial($s0)
 	addi $s1, $s1, 384
 	sw $s1, frogInitial($s0)
-	addi $s0, $s0, 4
-	j stMoveDown
 	
-initJump20:
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+	
 	jr $ra
 	
 	
 moveRight:
-	add $s0, $zero, $zero
-	add $s1, $zero, $zero
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
 	
-stMoveRight:
-	beq $s0, 4, initJump21
+	add $s1, $zero, $zero
 	lw $s1, frogInitial($s0)
 	addi $s1, $s1, 4
 	sw $s1, frogInitial($s0)
-	addi $s0, $s0, 4
-	j stMoveRight
 	
-initJump21:
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+	
 	jr $ra
 	
 
@@ -202,9 +207,322 @@ initJump21:
 ######################## Check keyboard inputs end.######################################	
 
 ######################## Collision detect. ################################################
+collision:
+	lw $s0, frogInitial($zero)
+
+# frog is below the start zone
+collisionCase1:
+	slti $s1, $s0, 3456
+	beq $s1, 1, collisionCase2
+	j unHit
+	
+# frog is in the car zone
+collisionCase2:
+	sgt $s1, $s0, 2172
+	beq $s1, 1, roadCollision
+	
+	
+# frog is in the safe zone in the middle
+collisionCase3:
+	slti $s1, $s0, 1792
+	beq $s1, 1, collisionCase4
+	j unHit
+
+# frog is in the water zone
+collisionCase4:
+	sgt $s1, $s0, 508
+	beq $s1, 1, waterCollision
+	# above goal zone
+	j goalCollision
+	
+
+roadCollision:
+	lw $s0, frogInitial($zero)
+	add $s0, $s0, $t0
+	lw $s2, 0($s0)
+	beq $t1, $s2, fail
+	
+	j unHit
+	
+	
+waterCollision:
+	lw $s0, frogInitial($zero)
+	add $s0, $s0, $t0
+	lw $s2, 0($s0)
+	beq $t3, $s2, fail
+	
+	j unHit
+	
+goalCollision:
+	lw $s0, frogInitial($zero)
+	add $s0, $s0, $t0
+	
+	addi $s1, $t0, 268
+	addi $s2, $t0, 272
+	addi $s3, $t0, 396
+	addi $s4, $t0, 400
+	beq $s0, $s1, hitGoal1
+	beq $s0, $s2, hitGoal1
+	beq $s0, $s3, hitGoal1
+	beq $s0, $s4, hitGoal1
+	
+	addi $s1, $t0, 292
+	addi $s2, $t0, 296
+	addi $s3, $t0, 420
+	addi $s4, $t0, 424
+	beq $s0, $s1, hitGoal2
+	beq $s0, $s2, hitGoal2
+	beq $s0, $s3, hitGoal2
+	beq $s0, $s4, hitGoal2
+	
+	addi $s1, $t0, 316
+	addi $s2, $t0, 320
+	addi $s3, $t0, 444
+	addi $s4, $t0, 448
+	beq $s0, $s1, hitGoal3
+	beq $s0, $s2, hitGoal3
+	beq $s0, $s3, hitGoal3
+	beq $s0, $s4, hitGoal3
+	
+	addi $s1, $t0, 340
+	addi $s2, $t0, 344
+	addi $s3, $t0, 468
+	addi $s4, $t0, 472
+	beq $s0, $s1, hitGoal4
+	beq $s0, $s2, hitGoal4
+	beq $s0, $s3, hitGoal4
+	beq $s0, $s4, hitGoal4
+	
+	addi $s1, $t0, 364
+	addi $s2, $t0, 368
+	addi $s3, $t0, 492
+	addi $s4, $t0, 496
+	beq $s0, $s1, hitGoal5
+	beq $s0, $s2, hitGoal5
+	beq $s0, $s3, hitGoal5
+	beq $s0, $s4, hitGoal5
+	
+	j fail
+	
+	
+fail:
+	lw $s0, lives($zero)
+	addi $s0, $s0, -1
+	sw $s0, lives($zero)
+	beq $s0, 0, Exit
+	addi $s5, $zero, 1
+	jr $ra
+
+hit:
+	addi $s5, $zero, 1
+	jr $ra
+	
+unHit:
+	add $s5, $zero, $zero
+	jr $ra
+	
+returnToInitial:
+	
+	lw $s0, frogInitial($zero)
+	addi $s0, $zero, 3508
+	sw $s0, frogInitial($zero)
+	
+	j refreshFrog
+	
+	
+hitGoal1:
+	j paintGoal1
+hitGoal2:
+	j paintGoal2
+hitGoal3:
+	j paintGoal3
+hitGoal4:
+	j paintGoal4
+hitGoal5:
+	j paintGoal5
+
+
+paintGoal1:
+	
+	sw $t5, 268($t0)
+	sw $t5, 272($t0)
+	sw $t5, 396($t0)
+	sw $t5, 400($t0)
+	
+	lw $s0, goal($zero)
+	addi $s0, $zero, 1
+	sw $s0, goal($zero)
+	
+	j hit
+
+paintGoal2:
+
+	sw $t5, 292($t0)
+	sw $t5, 296($t0)
+	sw $t5, 420($t0)
+	sw $t5, 424($t0)
+	
+	addi $s1, $zero, 4
+	lw $s0, goal($s1)
+	addi $s0, $zero, 1
+	sw $s0, goal($s1)
+	
+	j hit
+
+paintGoal3:
+
+	sw $t5, 316($t0)
+	sw $t5, 320($t0)
+	sw $t5, 444($t0)
+	sw $t5, 448($t0)
+	
+	addi $s1, $zero, 8
+	lw $s0, goal($s1)
+	addi $s0, $zero, 1
+	sw $s0, goal($s1)
+	
+	j hit
+
+paintGoal4:
+	
+	sw $t5, 340($t0)
+	sw $t5, 344($t0)
+	sw $t5, 468($t0)
+	sw $t5, 472($t0)
+	
+	addi $s1, $zero, 12
+	lw $s0, goal($s1)
+	addi $s0, $zero, 1
+	sw $s0, goal($s1)
+	
+	j hit
+	
+paintGoal5:
+	
+	sw $t5, 364($t0)
+	sw $t5, 368($t0)
+	sw $t5, 492($t0)
+	sw $t5, 498($t0)
+	
+	addi $s1, $zero, 16
+	lw $s0, goal($s1)
+	addi $s0, $zero, 1
+	sw $s0, goal($s1)
+	
+	j hit
+	
 
 
 ######################## Collision detect ends. ###########################################
+
+
+################################## paint life ###########################################
+paintLife3:
+	sw $t5, 3840($t0)
+	sw $t5, 3844($t0)
+	sw $t5, 3968($t0)
+	sw $t5, 3972($t0)
+	
+	sw $t7, 3848($t0)
+	sw $t7, 3976($t0)
+	# 
+	sw $t5, 3852($t0)
+	sw $t5, 3856($t0)
+	sw $t5, 3980($t0)
+	sw $t5, 3984($t0)
+
+	sw $t7, 3860($t0)
+	sw $t7, 3988($t0)
+	
+	#
+	sw $t5, 3864($t0)
+	sw $t5, 3992($t0)
+	sw $t5, 3868($t0)
+	sw $t5, 3996($t0)
+	
+	j refreshFrog
+
+paintLife2:
+	sw $t5, 3840($t0)
+	sw $t5, 3844($t0)
+	sw $t5, 3968($t0)
+	sw $t5, 3972($t0)
+	
+	sw $t7, 3848($t0)
+	sw $t7, 3976($t0)
+	# 
+	sw $t5, 3852($t0)
+	sw $t5, 3856($t0)
+	sw $t5, 3980($t0)
+	sw $t5, 3984($t0)
+
+	sw $t7, 3860($t0)
+	sw $t7, 3988($t0)
+	
+	#
+	sw $t7, 3864($t0)
+	sw $t7, 3992($t0)
+	sw $t7, 3868($t0)
+	sw $t7, 3996($t0)
+	
+	j refreshFrog
+
+
+paintLife1:
+	
+	sw $t5, 3840($t0)
+	sw $t5, 3844($t0)
+	sw $t5, 3968($t0)
+	sw $t5, 3972($t0)
+	
+	sw $t7, 3848($t0)
+	sw $t7, 3976($t0)
+	# 
+	sw $t7, 3852($t0)
+	sw $t7, 3856($t0)
+	sw $t7, 3980($t0)
+	sw $t7, 3984($t0)
+
+	sw $t7, 3860($t0)
+	sw $t7, 3988($t0)
+	
+	#
+	sw $t7, 3864($t0)
+	sw $t7, 3992($t0)
+	sw $t7, 3868($t0)
+	sw $t7, 3996($t0)
+	
+	j refreshFrog
+
+paintLife0:
+
+	sw $t7, 3840($t0)
+	sw $t7, 3844($t0)
+	sw $t7, 3968($t0)
+	sw $t7, 3972($t0)
+	
+	sw $t7, 3848($t0)
+	sw $t7, 3976($t0)
+	# 
+	sw $t7, 3852($t0)
+	sw $t7, 3856($t0)
+	sw $t7, 3980($t0)
+	sw $t7, 3984($t0)
+
+	sw $t7, 3860($t0)
+	sw $t7, 3988($t0)
+	
+	#
+	sw $t7, 3864($t0)
+	sw $t7, 3992($t0)
+	sw $t7, 3868($t0)
+	sw $t7, 3996($t0)
+	
+	j refreshFrog
+
+################################## paint life ends ###########################################
+
+
 
 
 ####################### Refresh the screen. ###################################################
@@ -227,6 +545,57 @@ refresh:
 	jal initCar2Var
 	jal initCar3Var
 	
+	# detect collision
+	jal collision
+	
+	beq $s5, 1, returnToInitial
+	
+refreshGoal1:
+	lw $s0, goal($zero)
+	beq $s0, 1, paintGoal1
+	
+	
+refreshGoal2:
+	addi $s1, $zero, 4
+	lw $s0, goal($s1)
+	beq $s0, 1, paintGoal2
+	
+refreshGoal3:
+	addi $s1, $zero, 8
+	lw $s0, goal($s1)
+	beq $s0, 1, paintGoal3
+	
+refreshGoal4:
+	addi $s1, $zero, 12
+	lw $s0, goal($s1)
+	beq $s0, 1, paintGoal4
+	
+refreshGoal5:
+	addi $s1, $zero, 16
+	lw $s0, goal($s1)
+	beq $s0, 1, paintGoal5
+	
+refreshLife3:
+
+	lw $s0, lives($zero)
+	beq $s0, 3, paintLife3
+	
+refreshLife2:
+
+	lw $s0, lives($zero)
+	beq $s0, 2, paintLife2
+	
+refreshLife1:
+	
+	lw $s0, lives($zero)
+	beq $s0, 1, paintLife1
+
+
+refreshLife0:
+	lw $s0, lives($zero)
+	beq $s0, 0, paintLife0
+	
+refreshFrog:
 	jal drawFrog
 	
 	li $v0, 32
@@ -253,6 +622,7 @@ drawStaticBg:
 	jal initRoad	 # draw the road area
 	jal initVar2     # draw the safe area on the bottom
 	jal printLife    # draw the lifes on the bottom
+	jal printBlack
 	
 	lw $ra, 0($sp)
 	addi $sp, $sp, 4
@@ -429,6 +799,26 @@ printLife:
 	sw $t5, 3992($t0)
 	sw $t5, 3996($t0)
 	
+	jr $ra
+	
+	
+	
+printBlack:
+	add $s5, $zero, $zero
+	addi $s6, $zero, 256
+	add $s7, $t0, 3840
+
+stPrintBlack:
+	beq $s5, $s6, initJump20
+	sw $t7, 0($s7)
+
+	
+upPrintBlack:
+	addi $s5, $s5, 4
+	addi $s7, $s7, 4
+	j stPrintBlack
+	
+initJump20:
 	jr $ra
 	
 ######################## Draw the static background ends.######################################
@@ -724,7 +1114,7 @@ stPaintCar1:
 	beq $s0, 96, initJump11
 	lw $s1, car1Initial($s0)
 	add $s1, $s1, $t0
-	sw $t2, 0($s1)
+	sw $t1, 0($s1)
 	addi $s0, $s0, 4
 	
 	j stPaintCar1
@@ -740,7 +1130,7 @@ stPaintCar2:
 	beq $s0, 96, initJump12
 	lw $s1, car2Initial($s0)
 	add $s1, $s1, $t0
-	sw $t2, 0($s1)
+	sw $t1, 0($s1)
 	addi $s0, $s0, 4
 	
 	j stPaintCar2
@@ -770,10 +1160,18 @@ initJump13:
 ######################## Draw the frog.                  ######################################
 
 drawFrog:
+
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	
 	add $s0, $zero, $zero
 	lw $s1, frogInitial($s0)
 	add $s1, $s1, $t0
 	sw $t5, 0($s1)
+	
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+	
 	jr $ra
 	
 ######################## Draw the frog ends.             ######################################
